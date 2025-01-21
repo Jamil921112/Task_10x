48ea398255724fcbbc1326cb79da44d60c178280
jamil92112 <arshad241297@gmail.com>
2025-01-21 15:28:47 +0530

    Initial commit

---
+++ src/ex_terop_practice/Lab_011_TO_even_odd.java
@@ -0,0 +1,9 @@
+package ex_terop_practice;
+
+public class Lab_011_TO {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/ex_terop_practice/Lab_three_largest_number.java
@@ -0,0 +1,4 @@
+package ex_terop_practice;
+
+public class Lab_three {
+}
