6842bb1ffb1504e44211c83f473e479a1d98d3dc
jamil92112 <arshad241297@gmail.com>
2025-01-23 11:49:19 +0530

    %dtable9

--- src/ex_QnA_Table9/Lab_021table_9.java
+++ src/ex_QnA_Table9/Lab_021table_9.java
@@ -5,14 +5,14 @@ public class Lab_021table_9 {
         int n = Integer.parseInt(args[0]);
         System.out.printf( "%dx1=%d",n,n*1 ).println();
         System.out.printf( "%dx2=%d",n,n*2 ).println();
-        System.out.printf( "%dx1=%d",n,n*3 ).println();
-        System.out.printf( "%dx1=%d",n,n*4 ).println();
-        System.out.printf( "%dx1=%d",n,n*5 ).println();
-        System.out.printf( "%dx1=%d",n,n*6 ).println();
-        System.out.printf( "%dx1=%d",n,n*7).println();
-        System.out.printf( "%dx1=%d",n,n*8 ).println();
-        System.out.printf( "%dx1=%d",n,n*9 ).println();
-        System.out.printf( "%dx1=%d",n,n*10 ).println();
+        System.out.printf( "%dx3=%d",n,n*3 ).println();
+        System.out.printf( "%dx4=%d",n,n*4 ).println();
+        System.out.printf( "%dx5=%d",n,n*5 ).println();
+        System.out.printf( "%dx6=%d",n,n*6 ).println();
+        System.out.printf( "%dx7=%d",n,n*7).println();
+        System.out.printf( "%dx8=%d",n,n*8 ).println();
+        System.out.printf( "%dx9=%d",n,n*9 ).println();
+        System.out.printf( "%dx10=%d",n,n*10 ).println();
 
 
 
