03962a9c67c8638aa620289a3fcb332c8a170eb4
jamil92112 <arshad241297@gmail.com>
2025-01-23 11:39:17 +0530

    table9

---
+++ src/Lab_pre_Incr.java
@@ -0,0 +1,10 @@
+public class Lab_pre_Incr {
+ public static void main(String[] args) {
+  int a = 10;
+  int b = ++a;
+  System.out.println(a);          // Line /Exp a /  exp b
+  System.out.println(b);          // 3   / 10   / NA
+                                  // 4  /11    /11
+ }
+    
+ }
---
+++ src/ex_increment_decrement/Lab_014_pre.java
@@ -0,0 +1,9 @@
+package ex_increment_decrement;
+
+public class Lab_014_pre {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a);
+        System.out.println(a);
+    }
+}
