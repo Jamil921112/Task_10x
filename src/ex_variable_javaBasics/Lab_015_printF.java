8639da064d6d602aa7af12d0965cd44a6b5c48b5
jamil921112 <arshad241297@gmail.com>
2025-05-15 23:31:11 +0530

    15/5/2025 variables

---
+++ src/ex_variable_javaBasics/Lab_015_printF.java
@@ -0,0 +1,16 @@
+package ex_variable_javaBasics;
+
+public class Lab_015_printF {
+    public static void main(String[] args) {
+        int a=10;System.out.printf("Variable name is%d",a);
+      // %d - integer , short,  long
+//     %s ->
+//    % f -> float , %b -> boolean
+
+
+
+
+
+
+}
+}
---
+++ src/ex_variable_javaBasics/lab_019Multiple_classes.java
@@ -0,0 +1,13 @@
+package ex_variable_javaBasics;
+
+public class lab_019Multiple_classes {
+    public static void main(String[] args) {
+        class A{
+
+        }
+        class B{
+
+        }
+        System.out.println();
+    }
+}
