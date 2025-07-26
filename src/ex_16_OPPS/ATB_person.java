6a1510513a1818fbb08dd808ad74cb86f2745818
jamil921112 <arshad241297@gmail.com>
2025-07-26 12:45:32 +0530

    23/7/25 Array&2D Array

---
+++ src/ex_16_OPPS/ATB_person.java
@@ -0,0 +1,18 @@
+package ex_16_OPPS;
+
+public class ATB_person {
+
+    //Attributes
+    String name ;
+    int age;
+    String gender ;
+    int salary;
+    long phone ;
+    String address ;
+
+    // Behaviour
+    void  speak(){}
+     void   eat (){}
+       void sleep (){}
+
+}
