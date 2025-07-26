115f17bae38a1c922675bb446e835c3f39619011
jamil921112 <arshad241297@gmail.com>
2025-07-26 14:32:10 +0530

    23/7/25 Array&2D Array

---
+++ src/ex_14_String_Function/String_54.java
@@ -0,0 +1,10 @@
+package ex_14_String_Function;
+
+public class String_54 {
+    public static void main(String[] args) {
+        String str = "Aeroplane";
+        String sub = str.substring(6,9);
+        System.out.println(sub);
+
+    }
+}
