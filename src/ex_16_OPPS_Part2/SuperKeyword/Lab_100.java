0234982e97f093483677d10fda36cc8094282422
jamil921112 <arshad241297@gmail.com>
2025-09-03 22:44:26 +0530

    Access Super

---
+++ src/ex_16_OPPS_Part2/SuperKeyword/Lab_100.java
@@ -0,0 +1,22 @@
+package ex_16_OPPS_Part2.SuperKeyword;
+
+public class Lab_100 {
+    public static void main(String[] args) {
+        Son S1 = new Son();
+        S1.t1();
+    }
+
+}
+ class Father {
+    int gold =10;
+    void Home(){
+        System.out.println("Home Father ");
+
+    } // super keyword ka kaam apne father ka methods ,attributes access karna
+ }
+  class  Son extends Father{
+    void t1 (){
+        super.Home();
+        System.out.println(super.gold);
+    }
+  }
